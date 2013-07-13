99252d237b2962f9ee2dd6a0f917a6ae393c22b2
Andre Ambrosio <andre.ambrosio@me.com>
2013-07-13 16:27:22 +1000

    correct pb 14

--- problems/pb14.rb
+++ problems/pb14.rb
@@ -1,6 +1,5 @@
 def collatz(n)
-  i = 0
-  n.even? ? i = (n / 2) : i = ( 3 * n + 1)
+  return n.even? ? (n / 2) : ( 3 * n + 1)
 end
 
 def collatz_sequence(n)
@@ -16,7 +15,7 @@ end
 def highest_collatz_seq(n)
   highest = 0
   number = n
-  while n < 1000000
+  while n < 10000
     curr = collatz_sequence(n)
     if curr > highest
       highest = curr
@@ -28,5 +27,5 @@ def highest_collatz_seq(n)
   return number
 end
        
-puts highest_collatz(500000)
+puts highest_collatz_seq(100)
     
