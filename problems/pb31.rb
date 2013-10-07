2967fd4517a830f8d702fb85d314ce3bca3f4a3f
Andre Ambrosio <andre.ambrosio@me.com>
2013-10-07 14:01:14 +1100

    Problem 31 solved

--- problems/pb31.rb
+++ problems/pb31.rb
@@ -1,20 +1,34 @@
- coins = [0,1,2,5,10,20,50,100,200]
+coins =  [1,2,5,10,20,50,100,200]
+counts = [200,100,40,20,10,4,2,1]
 
-# combinations = coins.repeated_combination(200).to_a.select{ |e| e.reduce(:+)==200}
+h = Hash[coins.zip counts]
+possibilities = 0
 
-# puts combinations.to_s
-# puts combinations.length
-count = 0
-coins.repeated_permutation(200).each do |p|
-	sum = 0
-	p.each do |i|
-		sum += i
-		if sum > 200
-			break
-		end
-	end
-	if sum == 200
-		count +=1
-	end
+h[200].downto(0) do |i|
+  h[100].downto(0) do |j|
+    next if 200*i+100*j > 200
+    h[50].downto(0) do |k|
+      next if 200*i+100*j+50*k > 200
+      h[20].downto(0) do |l|
+        next if 200*i+100*j+50*k+20*l > 200
+        h[10].downto(0) do |m|
+          next if 200*i+100*j+50*k+20*l+10*m > 200
+          h[5].downto(0) do |n|
+            next if 200*i+100*j+50*k+20*l+10*m+5*n > 200
+            h[2].downto(0) do |o|
+              next if 200*i+100*j+50*k+20*l+10*m+5*n+2*o > 200
+              h[1].downto(0) do |p|
+                sum = p+2*o+5*n+10*m+20*l+50*k+100*j+200*i
+                if sum == 200
+                  possibilities+=1
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
-puts count
+
+puts possibilities
