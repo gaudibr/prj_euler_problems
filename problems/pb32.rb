2f7cddabea29d3fb1c4495a4ac6b3e671c965e8e
Andre Ambrosio <andre.ambrosio@me.com>
2013-10-07 22:36:31 +1100

    problem 32 solved

--- problems/pb32.rb
+++ problems/pb32.rb
@@ -5,27 +5,15 @@ class Array
 
     if b.split('').uniq.length == 9
       return true
-    end if b.length == 9
-    return false
+    end if b.length == 9 && /0/!~b
   end
 end
 
 
 products = []
 
-for i in 10..99
-  for j in 100..999
-    a = []
-    p = i*j
-    a = [i.to_s,j.to_s,p.to_s]
-    if a.pandigital?
-      products << a
-    end
-  end
-end
-
-for i in 1..9
-  for j in 1000..9999
+for i in 1..99
+  for j in 50..9999
     a = []
     p = i*j
     a = [i.to_s,j.to_s,p.to_s]
@@ -37,4 +25,4 @@ end
 
 results = []
 products.map { |product| results << product[2].to_i}
-puts results.uniq.reduce(:+)
+puts results.uniq.sort.reduce(:+)
