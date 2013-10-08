96da5d5e752eac625ef576dadbd2913bae7bb7a8
Andre Ambrosio <andre.ambrosio@me.com>
2013-10-08 12:00:24 +1100

    solved pb33

--- problems/pb33.rb
+++ problems/pb33.rb
@@ -1,3 +1,5 @@
+require 'Prime'
+
 a = []
 for i in 10..99
   for j in 10..99
@@ -7,6 +9,7 @@ for i in 10..99
 end
 
 results = []
+
 a.each do |b|
   s1 = b[0].to_s.split('')
   s2 = b[1].to_s.split('')
@@ -15,16 +18,24 @@ a.each do |b|
     for l in 1..2
       n = s1[k-1].to_f
       d = s2[l-1].to_f
+      n1 = s1[k%2].to_f
+      d1 = s2[l%2].to_f
 
-      r1 = s1[k%2]
-      r2 = s2[l%2]
-      puts "1:",b.to_s
-      puts "2:", n.to_s,d.to_s,r1,r2
-      if n/d == b[0].to_f/b[1].to_f && r1==r2
-        results << [b,[n,d]]  if (b[0].to_i % n.to_i ) != 0
+      q0 = b[0].to_f/b[1].to_f
+      q = n/d if d!=0
+
+
+
+      if (q0 == q) && (n1==d1) && (q < 1) && (n1 != 0)
+        results << [b,[n,d],[n1,d1]]
       end if d !=0
     end
   end
 end
 
+puts results.map {|e| e[1][0].to_i}.inject(:lcm)
+puts results.map {|e| e[1][1].to_i}.inject(:lcm)
+
 puts results.to_s
+
+#the actual result is the denominator of the fraction resulting from the multiplication of the four fractions in their lowest possible terms (simplified)
