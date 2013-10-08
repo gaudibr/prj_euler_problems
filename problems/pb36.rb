bdc8782538c252829146f0a93ab9139973a3609a
Andre Ambrosio <andre.ambrosio@me.com>
2013-10-08 16:48:41 +1100

    problem 36 enhanced

--- problems/pb36.rb
+++ problems/pb36.rb
@@ -1,26 +1,22 @@
 require 'benchmark'
 
-class Integer
-  def reverse(base=10)
-    r = self.to_s.reverse.to_i
-  end
-end
-
-def double_base_palindromes n
+# @param [Integer] n : upper limit of the search for double based palindromes
+# @return [Array]  : array with all double based palindromes found
+def double_base_palindromes (n)
   dbp = []
   (1..n).each { |i|
     if i.odd?
       begin
         s = i.to_s
         sb = i.to_s(2)
-        ib = s.to_i
+
 
         dbp << i if s==s.reverse && sb==sb.reverse
 
       end
     end
   }
-  return dbp
+  dbp
 end
 
 puts Benchmark.measure {puts double_base_palindromes(1000000).reduce(:+)}
