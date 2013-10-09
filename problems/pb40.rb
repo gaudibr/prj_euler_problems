81abbfc99edb8df45be2f9b9847885ca13859d7d
Andre Ambrosio <andre.ambrosio@me.com>
2013-10-09 17:45:32 +1100

    added benchmark to pb 40

--- problems/pb40.rb
+++ problems/pb40.rb
@@ -1,13 +1,19 @@
-s = "0."
-i = 1
-while s.length <=1000002 do
-  s.concat(i.to_s)
-  i+=1
-end
-prod = 1
-for i in 0..6
-  dn = s[10**i+1]
-  prod *= dn.to_i
+require 'benchmark'
+
+def pb40
+  s = "0."
+  i = 1
+  while s.length <=1000002 do
+    s.concat(i.to_s)
+    i+=1
+  end
+  prod = 1
+  for i in 0..6
+    dn = s[10**i+1]
+    prod *= dn.to_i
+  end
+
+  puts prod
 end
 
-puts prod
+puts Benchmark.measure{pb40}
