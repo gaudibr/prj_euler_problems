c803f9dfeb1418023198d3ea9d7cbac227f645cb
Andre Ambrosio <andre.ambrosio@me.com>
2013-10-19 16:52:25 +1100

    problem 58 solved

--- problems/lib/pb58.rb
+++ problems/lib/pb58.rb
@@ -1,8 +1,29 @@
 require 'benchmark'
-require 'prime'
+require 'Prime'
 
-(1..(7**2)).to_a.select{|x| x.prime?}.length
+
+def spiral_primes
+  diag_a = [1]
+  primes = []
+  n=3
+  ratio = 1
+
+  begin
+    (0..3).each { |j|
+      a = (n**2 - j*(n-1))
+      primes << a if a.prime?
+      diag_a << a
+    }
+    ratio =  primes.length.to_f/diag_a.length
+    puts "side: #{n}, ratio: #{ratio}"
+    n+=2
+  end while ratio >=0.1
+
+end
+
+
 def pbx
+  spiral_primes
 end
 
 puts Benchmark.measure { pbx }
